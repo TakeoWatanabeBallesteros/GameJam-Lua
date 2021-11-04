858364bbd92a478f9993f131febfa76c27b33664
acanasr <77283169+acanasr@users.noreply.github.com>
2021-11-04 23:49:55 +0100

    minijuego vodka
    inacabado

---
+++ Game/Minijuego_Vodka/DrinkingGame.lua
@@ -0,0 +1,136 @@
+Actor = Actor or require "Scripts/actor"
+Timer = Timer or require "Scripts/Timer"
+CameraShake = CameraShake or require "Scripts/CameraShake"
+DrinkingGame = Actor:extend()
+local background = {}
+slider = {}
+playerBar = {}
+local moveState, lastMoveState
+local speed
+local shots
+local t,c
+
+function DrinkingGame:new()
+  background.img0 = love.graphics.newImage("data/mjvodkaGameTextures/0shots.png")
+  background.img1 = love.graphics.newImage("data/mjvodkaGameTextures/1shots.png")
+  background.img2 = love.graphics.newImage("data/mjvodkaGameTextures/2shots.png")
+  background.img3 = love.graphics.newImage("data/mjvodkaGameTextures/3shots.png")
+  background.img4 = love.graphics.newImage("data/mjvodkaGameTextures/4shots.png")
+  background.img5 = love.graphics.newImage("data/mjvodkaGameTextures/5shots.png")
+  background.img6 = love.graphics.newImage("data/mjvodkaGameTextures/6shots.png")
+  background.img7 = love.graphics.newImage("data/mjvodkaGameTextures/7shots.png")
+  background.img8 = love.graphics.newImage("data/mjvodkaGameTextures/8shots.png")
+  background.img9 = love.graphics.newImage("data/mjvodkaGameTextures/9shots.png")
+  background.imgDrinking = love.graphics.newImage("data/mjvodkaGameTextures/Drinking.png")
+  background.imgShown = background.img
+  --background.laTexturesr = 1
+  
+  slider.img = love.graphics.newImage("data/mjvodkaGameTextures/Slider.png")
+  
+  slider.ox = slider.img:getWidth()/2
+  slider.oy = slider.img:getHeight()/2
+  slider.x = 300+slider.ox
+  slider.y = 530
+  --slider.laTexturesr = 2
+  
+  playerBar.img = love.graphics.newImage("data/mjvodkaGameTextures/playerBar.png")
+  playerBar.x = slider.x
+  playerBar.y = slider.y
+  playerBar.ox = playerBar.img:getWidth()/2
+  playerBar.oy = playerBar.img:getHeight()/2
+  
+  shots = 0
+  speed = 700
+  self.points = 0
+  
+end
+function DrinkingGame:update(dt)
+  print(self.points.."     "..shots)
+  if moveState == 1 then playerBar.x = playerBar.x + speed*dt end
+  if moveState == 2 then playerBar.x = playerBar.x - speed*dt end
+  if moveState == 3 then background.imgShown = background.imgDrinking
+  else 
+    if shots == 0 then background.imgShown = background.img0 end
+    if shots == 1 then background.imgShown = background.img1 end
+    if shots == 2 then background.imgShown = background.img2 end
+    if shots == 3 then background.imgShown = background.img3 end
+    if shots == 4 then background.imgShown = background.img4 end
+    if shots == 5 then background.imgShown = background.img5 end
+    if shots == 6 then background.imgShown = background.img6 end
+    if shots == 7 then background.imgShown = background.img7 end
+    if shots == 8 then background.imgShown = background.img8 end
+    if shots > 8 then background.imgShown = background.img9 end
+    end
+  
+  if playerBar.x < slider.x + 2 then 
+    playerBar.x = slider.x
+    moveState = 1
+  end
+  
+  if playerBar.x > slider.x + slider.ox*2 + 2 then 
+    playerBar.x = slider.x + slider.ox*2
+    moveState = 2
+  end
+    
+    
+    
+    if love.keyboard.isDown("space") then
+      if moveState == 1 or moveState == 2 then
+      shots = shots+1
+      lastMoveState = moveState
+      moveState = 3
+      speed = speed +220
+      if playerBar.x > slider.x+slider.img:getWidth()*0.4 and playerBar.x < slider.x+slider.img:getWidth()*0.6 then
+        self.points = self.points+3
+        --c = CameraShake(.2,.4)
+        --table.insert(actorList, c)
+      else
+        self.points = self.points -1
+        --c = CameraShake(.3,1)
+        --table.insert(actorList, c)
+      end
+      
+      t = Timer(3, function() 
+          moveState = lastMoveState
+        end, false)
+      table.insert(actorList,t)
+      end
+      
+    end
+    
+    
+end
+function DrinkingGame:draw()
+  love.graphics.draw(background.imgShown,80,80,0,.9,.9,0,0,0,0)
+  if (moveState == 1 or moveState == 2) then love.graphics.draw(slider.img,slider.x,slider.y,0,1,1,slider.ox,slider.oy,0,0) end
+  if (moveState == 1 or moveState == 2) then love.graphics.draw(playerBar.img,playerBar.x,playerBar.y,0,1,1,slider.ox,slider.oy,0,0) end
+  
+end
+
+function DrinkingGame:keyPressed(key)
+  
+  if key == "space" then
+    if moveState == 1 or moveState == 2 then
+      
+      lastMoveState = moveState
+      moveState = 3
+      speed = speed +100
+      if playerBar.x > slider.x+slider.img:getWidth()*0.4 and playerBar.x < slider.x+slider.img:getWidth()*0.6 then
+        self.points = self.points+3
+        shots = shots+1
+        --c = CameraShake(.2,.4)
+        --table.insert(actorList, c)
+      else
+        self.points = self.points -1
+        --c = CameraShake(.3,1)
+        --table.insert(actorList, c)
+      end
+      
+      t = Timer(2, function() 
+          moveState = lastMoveState
+          end, false)
+      end
+      table.insert(actorList,t)
+    end
+  end
+return DrinkingGame
